7fe22bc72bf24206f1150aea6d8de878f01ef192
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2024-12-26 20:54:29 +0200

    Create Error.sql

---
+++ Error.sql
@@ -0,0 +1,9 @@
+--Created By Abdelwahab Shandy
+--Don't Forget To Pray For Me And My Parents
+--Error 
+--===========================
+--Cannot execute as the database principal because the principal "dbo" does not exist, this type of principal cannot be impersonated,or you do not have permission. (Framework Microsoft SqlClient Data Provider) 
+
+use [YourDatabaseName] EXEC sp_changedbowner 'sa' 
+
+--===========================
