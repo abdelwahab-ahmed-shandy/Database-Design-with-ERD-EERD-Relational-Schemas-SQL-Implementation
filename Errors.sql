2d7cef55a56f99229d54c8fec379111b690e1f86
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2024-12-27 17:08:31 +0200

    Update Errors.sql

--- Errors.sql
+++ Errors.sql
@@ -2,6 +2,10 @@
 --Don't Forget To Pray For Me And My Parents
 --Error 
 --===========================
+From  =>> Where  =>> Select  =>> Order By
+
+
+--===========================
 --Cannot execute as the database principal because the principal "dbo" does not exist, this type of principal cannot be impersonated,or you do not have permission. (Framework Microsoft SqlClient Data Provider) 
 
 use [YourDatabaseName] EXEC sp_changedbowner 'sa' 
